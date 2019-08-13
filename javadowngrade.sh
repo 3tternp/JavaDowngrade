3fce0144130f75ced32e4ca15c403e08c75f6386
astra <basnetprem1234@gmail.com>
2019-08-13 10:25:36 +0545

    Update javadowngrade.sh

--- javadowngrade.sh
+++ javadowngrade.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-echo -e "\e[0;36;34m Welcom3 in Java Downgrade Process!!"
-echo -e "\e[0;36;30m Please Enter the number which version you want to Downgrade: "
+echo -e "\e[32m Welcom3 in Java Downgrade Process!!"
+echo -e "\e[92m Please Enter the number which version you want to Downgrade: "
 sudo update-alternatives --config java
